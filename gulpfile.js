318377d9e6b4253b55046c202dfa00613441e838
moggers <mattkramara@gmail.com>
2016-08-17 17:10:34 +0800

    Fix gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,22 @@ var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 
+function handleError(err){
+    console.log("Failed to build")
+    console.log(err.toString());
+    this.emit('end');
+}
+
 gulp.task('default', function() {
-    console.log("Building");
     return gulp.src("src/*.js")
         .pipe(sourcemaps.init())
         .pipe(babel())
+        .on("error", handleError)
         .pipe(concat("all.js"))
         .pipe(sourcemaps.write("."))
         .pipe(gulp.dest("dist"));
+
 });
 gulp.task('watch', function() {
     var watcher = gulp.watch('src/*.js', ['default']);
-    watcher.on('change', function(event) {
-        console.log('file' + event.path + ' was ' + event.type + ', running tasks..');
-    })
 });
